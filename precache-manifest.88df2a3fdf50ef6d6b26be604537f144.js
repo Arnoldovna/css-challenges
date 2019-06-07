da2735c7404323fbae9f9592bc1f6033aa473d90
Marina Wilhelm <marina.wilhelm@icloud.com>
2019-06-07 11:55:51 +0200

    Updates

---
+++ precache-manifest.88df2a3fdf50ef6d6b26be604537f144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e64666c78fa07867cdcbab216f6cc2",
+    "url": "/css-challenges/index.html"
+  },
+  {
+    "revision": "8ce0d059541735678678",
+    "url": "/css-challenges/static/css/main.da901791.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/css-challenges/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "8ce0d059541735678678",
+    "url": "/css-challenges/static/js/main.4955bd90.chunk.js"
+  },
+  {
+    "revision": "6e5cab3c656a06d19bd3",
+    "url": "/css-challenges/static/js/runtime~main.60514b97.js"
+  }
+]);
