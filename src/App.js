d698840087fcbcd895939d11f7ebdabc972fafba
Marina Wilhelm <marina.wilhelm@icloud.com>
2019-06-07 10:27:10 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>CSS CHALLENGE</h1>
+
+      <iframe
+        src='//codepen.io/Arnoldovna/embed/GabKJN/?height=800&amp;theme-id=22129&amp;default-tab=result'
+        frameborder='0'
+        scrolling='no'
+        allowfullscreen='true'
+        allowtransparency='true'
+        className='first'
+      />
     </div>
   );
 }
