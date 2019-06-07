5ee4d065f5c2b3f3e5fa67aa751e5041b71595bf
Marina Wilhelm <marina.wilhelm@icloud.com>
2019-06-07 14:39:41 +0200

    second iframe added

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3d7410ecfd14d6f3aef4d79a464a6ab.js"
+  "/css-challenges/precache-manifest.398170cacf501b27dd5429bac6434e72.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/css-challenges/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ function App() {
   return (
     <div className='App'>
       <h1>CSS CHALLENGE</h1>
-
       <iframe
         src='//codepen.io/Arnoldovna/embed/GabKJN/?height=800&amp;theme-id=22129&amp;default-tab=result'
         frameborder='0'
+        title='iframe'
         scrolling='no'
+        allowfullscreen='true'
+        allowtransparency='true'
+        className='first'
+      />
+
+      <iframe
+        src='//codepen.io/Arnoldovna/embed/OYrKoo/?height=800&amp;theme-id=22129&amp;default-tab=result'
+        frameborder='0'
+        scrolling='no'
+        title='iframe'
         allowfullscreen='true'
         allowtransparency='true'
         className='first'
