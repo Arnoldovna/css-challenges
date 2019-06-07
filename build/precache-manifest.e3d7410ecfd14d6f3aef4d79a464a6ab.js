cc51c342ff3477f30996a1111eab0876aa18a41d
Marina Wilhelm <marina.wilhelm@icloud.com>
2019-06-07 11:26:39 +0200

    build

---
+++ build/precache-manifest.e3d7410ecfd14d6f3aef4d79a464a6ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a305c439641167d1ba973fa6d9edd40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eba5e7ccdee2bbadcffb",
+    "url": "/static/css/main.da901791.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "eba5e7ccdee2bbadcffb",
+    "url": "/static/js/main.8754e465.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
