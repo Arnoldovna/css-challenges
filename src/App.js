7db5233e0fd25b93610e4ff72bd2e3b80a1462ae
Marina Wilhelm <marina.wilhelm@icloud.com>
2019-06-09 21:56:46 +0200

    favicon added, other iframes added, headline added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,185 @@
 import React from 'react';
-import journal from './journal.png';
+import journal from './journal.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className='App'>
-      <div className='headline'>
-        <img className='journal' src={journal} alt='journal' />
+      <div className='center'>
+        <div className='headline'>
+          <img className='journal' src={journal} alt='journal' />
+        </div>
+        <div className='grid'>
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/gJZVgX/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            title='challenge'
+            scrolling='no'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+            autoplay='false'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/VOqJJa/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            title='challenge'
+            scrolling='no'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/KYmXMW/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            title='challenge'
+            scrolling='no'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/pmqMrB/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/XQJZPo/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            title='challenge'
+            scrolling='no'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/xeEQbd/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            title='challenge'
+            scrolling='no'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/GabKJN/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            title='challenge'
+            scrolling='no'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/OYrKoo/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/agoOoj/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/qGLeOO/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            title='challenge'
+            scrolling='no'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/ZZOZmw/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/zQygzx/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/EzGqbV/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/MdZNBg/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/LoMwML/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/ZNNgZj/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/vwwovO/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+          <iframe
+            src='//codepen.io/Arnoldovna/embed/XwwvLo/?height=800&amp;theme-id=22129&amp;default-tab=result'
+            frameborder='0'
+            scrolling='no'
+            title='challenge'
+            allowfullscreen='true'
+            allowtransparency='true'
+            className='cell'
+          />
+        </div>
       </div>
-      <div className='grid' />
     </div>
   );
 }
