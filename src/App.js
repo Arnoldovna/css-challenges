c8ebd88f31d8b5323cb33cf1c53ce0926bea385a
Marina Wilhelm <marina.wilhelm@icloud.com>
2019-06-09 18:37:12 +0200

    journal headline added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-
+import journal from './journal.png';
 import './App.scss';
 
 function App() {
   return (
     <div className='App'>
-      <div className='header'>
-        <div className='background' />
-        <h1 className='css'>CSS</h1>
-        <h1 className='challenges'>Challenges</h1>
-        <h1 className='overview'>Overview</h1>
-        <svg className='triangle' x='0px' y='0px' viewBox='0 0 207.2 144'>
-          <polygon points='49.52,0 0,99.69 207.2,144 ' />
-        </svg>
-        <div className='circle' />
+      <div className='headline'>
+        <img className='journal' src={journal} alt='journal' />
       </div>
+      <div className='grid' />
     </div>
   );
 }
